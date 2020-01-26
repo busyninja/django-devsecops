457438be9ace6ec27e48d580ea3c970e9544d436
busyninja <mrk@busy.ninja>
2020-01-26 22:59:33 +0100

    fixing security vulnerability

--- devsecops/settings.py
+++ devsecops/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's3&cxadlx87xmari4n!@m#@q&_b07**s7tfb9ox3wwsk!js*rn'
+SECRET_KEY = os.environ.get('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
